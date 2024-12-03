4a603f7e4b7da08d01b2a9e24930985543e84f6c
michelle23-code <michellelekoto13@icloud.com>
2024-12-04 01:13:11 +0200

    Update Search.js

--- movie-search-app/src/Search.js
+++ movie-search-app/src/Search.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Search(handleInput,SearchResult) {
+function Search({handleInput, SearchResult}) {
   return (
     <div className='search-input my-3 mb-5'>
         <input type='text' name='movie' className='w-50 p-2'
